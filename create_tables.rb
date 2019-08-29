481f63d5510951c2664e23cd4a0b5a6c7d3df051
Nathan Woodhull <woodhull@gmail.com>
2019-08-29 17:49:26 -0400

    Add script to generate DDL from bulk data API

---
+++ create_tables.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'json'
+require 'byebug'
+
+def normalize_types(type)
+  if %w(hstore json jsonb).include?(type)
+    'varchar'
+  else
+    type
+  end
+
+end
+
+
+uri = URI('https://sign.moveon.org/api/bulk_data/schema.json')
+json = Net::HTTP.get(uri)
+tables = JSON.parse(json)['tables']
+
+tables.each do |table|
+  name = table['table']['name']
+  columns = table['table']['columns']
+  column_fields = columns.collect{|col, attrs| "\"#{col}\" #{normalize_types(attrs["sql_type"])}" }.join(', ')
+  puts "DROP TABLE IF EXISTS #{name};"
+  puts "CREATE TABLE #{name} (#{column_fields});"
+end
+
+
