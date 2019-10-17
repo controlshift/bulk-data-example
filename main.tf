ac623af3e91ce157f54bf01a9bab06a4a3a45772
Schuyler Duveen <sky@skyb.us>
2019-10-17 11:35:27 -0400

    received error that these were invalid variables

--- main.tf
+++ main.tf
@@ -6,13 +6,10 @@ module "terraform-aws-controlshift-redshift-sync" {
   manifest_bucket_name = var.manifest_bucket_name
   manifest_prefix = var.manifest_prefix
   failed_manifest_prefix = var.failed_manifest_prefix
-  success_topic_name = var.success_topic_name
-  failure_topic_name = var.failure_topic_name
   aws_region = var.aws_region
   redshift_database_name = var.redshift_database_name
   redshift_dns_name = var.redshift_dns_name
   redshift_port = var.redshift_port
   redshift_schema = var.redshift_schema
   controlshift_hostname = var.controlshift_hostname
-  receiver_timeout = var.receiver_timeout
 }
