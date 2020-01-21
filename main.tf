74cceab48b68e9c556b7f6aa57a5437173e0051c
anero <anero79@gmail.com>
2020-01-21 15:07:30 -0300

    Additional setup for signatures sync using AWS Glue

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
 module "terraform-aws-controlshift-redshift-sync" {
   source = "controlshift/controlshift-redshift-sync/aws"
-  redshift_username = var.redshift_username
-  redshift_password = var.redshift_password
-  receiver_bucket_name = var.receiver_bucket_name
+
+  aws_region = var.aws_region                                             // Default: "us-east-1"
+  controlshift_environment = var.controlshift_environment                 // Default: "production"
+  controlshift_hostname = var.controlshift_hostname                       // Default: "staging.controlshiftlabs.com"
+  controlshift_organization_slug = var.controlshift_organization_slug
+  failed_manifest_prefix = var.failed_manifest_prefix                     // Default: "failed"
+  failure_topic_name = var.failure_topic_name                             // Default: "ControlshiftLambdaLoaderFailure"
+  glue_scripts_bucket_name = var.glue_scripts_bucket_name
   manifest_bucket_name = var.manifest_bucket_name
-  manifest_prefix = var.manifest_prefix
-  failed_manifest_prefix = var.failed_manifest_prefix
-  aws_region = var.aws_region
-  redshift_database_name = aws_redshift_cluster.default.database_name
-  redshift_dns_name = aws_redshift_cluster.default.dns_name
-  redshift_port = aws_redshift_cluster.default.port
-  controlshift_environment = var.controlshift_environment
+  manifest_prefix = var.manifest_prefix                                   // Default: "manifests"
+  receiver_timeout = var.receiver_timeout                                 // Default: 60
+  redshift_cluster_identifier = var.redshift_cluster_identifier
+  redshift_database_name = var.redshift_database_name
+  redshift_password = var.redshift_password
+  redshift_schema = var.redshift_schema                                   // Default: "public"
+  redshift_security_group_id = var.redshift_security_group_id
+  redshift_subnet_id = var.redshift_subnet_id
+  redshift_username = var.redshift_username
+  success_topic_name = var.success_topic_name                             // Default: "ControlshiftLambdaLoaderSuccess"
 }
--- variables.tf
+++ variables.tf
@@ -3,29 +3,11 @@ variable "aws_region" {
   type        = string
   description = "The AWS Region to use. All resources will be created in this region."
 }
-variable "redshift_password" {
+
+variable "controlshift_environment" {
+  default = "staging"
   type        = string
-}
-variable "redshift_username" {
-  type        = string
-}
-variable "receiver_bucket_name" {
-  type        = string
-  description = "Your S3 bucket name ingest CSVs will be stored in"
-}
-variable "manifest_bucket_name" {
-  type        = string
-  description = "Your S3 bucket name to store manifests of ingests processed in"
-}
-variable "manifest_prefix" {
-  default = "manifests"
-  type        = string
-  description = "A file prefix that will be used for manifest logs on success"
-}
-variable "failed_manifest_prefix" {
-  default = "failed"
-  type        = string
-  description = "A file prefix that will be used for manifest logs on failure"
+  description = "The environment of your ControlShift instance. Either staging or production"
 }
 
 variable "controlshift_hostname" {
@@ -34,5 +16,80 @@ variable "controlshift_hostname" {
   description = "The hostname of your ControlShift instance. Likely to be something like action.myorganization.org"
 }
 
-variable "controlshift_environment" {
+variable "controlshift_organization_slug" {
+  type = string
+  description = "The organization's slug in ControlShift platform. Ask support team (support@controlshiftlabs.com) to find this value."
+}
+
+variable "glue_scripts_bucket_name" {
+  type        = string
+  description = "Your S3 bucket name to store Glue scripts in"
+}
+
+variable "failed_manifest_prefix" {
+  default = "failed"
+  type        = string
+  description = "A file prefix that will be used for manifest logs on failure"
+}
+
+variable "failure_topic_name" {
+  default = "ControlshiftLambdaLoaderFailure"
+  type        = string
+  description = "An SNS topic name that will be notified about batch processing failures"
+}
+
+variable "manifest_bucket_name" {
+  type        = string
+  description = "Your S3 bucket name to store manifests of ingests processed in"
+}
+
+variable "manifest_prefix" {
+  default = "manifests"
+  type        = string
+  description = "A file prefix that will be used for manifest logs on success"
+}
+
+variable "receiver_timeout" {
+  default = 60
+  type        = number
+  description = "The timeout for the receiving Lambda, in seconds"
+}
+
+variable "redshift_cluster_identifier" {
+  type = string
+  description = "The target Redshift cluster ID"
+}
+
+variable "redshift_database_name" {
+  type = string
+}
+
+variable "redshift_password" {
+  type  = string
+}
+
+variable "redshift_schema" {
+  type  = string
+  default = "public"
+  description = "The Redshift schema to load tables into"
+}
+
+variable "redshift_security_group_id" {
+  type = string
+  description = "The security group assigned to the Redshift cluster that will be used for connecting by Glue. For requirements on this Security Group see https://docs.aws.amazon.com/glue/latest/dg/setup-vpc-for-glue-access.html"
+}
+
+variable "redshift_subnet_id" {
+  type = string
+  description = "The ID of one of Redshift's cluster subnet group that Glue will use to connect"
+}
+
+variable "redshift_username" {
+  type = string
+}
+
+variable "success_topic_name" {
+  default = "ControlshiftLambdaLoaderSuccess"
+  type        = string
+  description = "An SNS topic name that will be notified about batch processing successes"
 }
